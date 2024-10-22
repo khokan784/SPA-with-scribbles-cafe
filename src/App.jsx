5cbe188615ba0c3fc23811c5d38aba4bdbe3109c
khokan <khokancrmt@gmail.com>
2024-10-22 12:19:32 +0600

    added file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/header/Header'
+import Blogs from './components/blogs/Blogs'
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl font-bold'>Knowlage Kafe</h1>
+        </div>
+    );
+};
+
+export default Header;
