6b92dc5f525a27068fef0c05e2834cfc153b7ad4
khokan <khokancrmt@gmail.com>
2024-10-22 13:16:16 +0600

    create blog section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+
 import './App.css'
 import Header from './components/header/Header'
 import Blogs from './components/blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 
 function App() {
   return (
     <>
       <Header></Header>
-      <Blogs></Blogs>
+      <div className="md:flex">
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
